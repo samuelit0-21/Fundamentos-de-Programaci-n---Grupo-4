d17a2ef8698096e7890d188cb736f99d47968886
Rodrigo <rodrigoalessandrg@gmail.com>
2026-09-01 21:06:46 -0500

    Create Ejercicio_1_Rodrigo.py
    Calculates the area and perimeter of a rectangle based on user input.

---
+++ Ejercicio 1/Ejercicio_1_Rodrigo.py
@@ -0,0 +1,15 @@
+#Consiguiendo el area y perimetro de un rectangúlo
+
+Base = float(input("Ingrese la base del rectángulo: "))
+
+Altura = float(input("Ingrese la altura del rectángulo: "))
+
+Area = Base * Altura
+
+Perimetro = (Base + Altura) * 2
+
+print ("El área del rectángulo es: ", Area)
+
+print ("El perímetro del rectángulo es: ", Perimetro)
+
+print ("El área y perímetro del rectángulo son: ", Area, Perimetro)
