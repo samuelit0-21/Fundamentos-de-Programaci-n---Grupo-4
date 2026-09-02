bdd8930de275c42985bc596f4ac2457947a22a22
Samuelito_Pro <jfryxd@gmail.com>
2026-09-01 21:09:50 -0500

    Update and rename Ejercicio2_Milla_Roldan.py to Ejercicio3_Milla_Roldan.py

---
+++ Ejercicio 3/Ejercicio3_Milla_Roldan.py
@@ -0,0 +1,6 @@
+#Ejercicio 3 - Tabla de Multiplicar
+
+n = int(input("Ingrese un numero, para mostrarle su tabla del 1 al 10: "))
+print (f"Tabla del {n}")
+for i in range (1,11):
+    print(f"{n}x{i}={n*i}")
