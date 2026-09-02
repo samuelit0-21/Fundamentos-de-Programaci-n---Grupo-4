6a84ef834f2095413a1065fdfd88c5e684283ae9
Dalessandro_Nina <nefergg3@gmail.com>
2026-09-01 21:12:57 -0500

    Ejercicio_2_Dalessandro_Nina.py

---
+++ Ejercicio 2 /Ejercicio_2_Dalessandro_Nina.py
@@ -0,0 +1,11 @@
+
+print("Bienvenido")
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+
+if num1 > num2:
+    print("El primer numero es el mayor")
+elif num2 > num1:
+    print("El segundo numero es el mayor")
+else:
+    print("Los dos numeros son iguales")
