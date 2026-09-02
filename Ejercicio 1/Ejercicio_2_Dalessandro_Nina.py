adba0f1d758bde71343e48eea829d0a1dbb1c18d
Dalessandro_Nina <nefergg3@gmail.com>
2026-09-01 21:14:18 -0500

    Ejercicio_1_Dalessandro_Nina.py

---
+++ Ejercicio 1/Ejercicio_2_Dalessandro_Nina.py
@@ -0,0 +1,9 @@
+
+print("************Bienvenido************")
+base = float(input("Ingrese la base: "))
+altura = float(input("Ingrese la altura: "))
+area = base*altura
+perimetro= (base*2)+(altura*2)
+
+print("El aréa de su rectangulo es: ",area)
+print("Y el perimetro de su rectangulo es: ",perimetro)
