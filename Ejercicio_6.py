79c6cf3507a649eae2d203bdad5600adab6117ab
Samuelito_Pro <jfryxd@gmail.com>
2026-09-01 20:21:20 -0500

    Delete Ejercicio_6.py

--- Ejercicio_6.py
+++
@@ -1,5 +0,0 @@
-#El usuario pone un número,  debe mostrar todos los números primos desde el numero 2, verifica si es posible dividir entre 2 y si tiene raiz cuadrada los numeros primos
-
-N1 = int(input("Ingrese un número: "))
-
-N = 
