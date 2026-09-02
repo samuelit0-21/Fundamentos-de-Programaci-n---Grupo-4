8abc5282d7cbac554296c43acae7259e30f4dfa0
André Pacheco <chiposcorpion@gmail.com>
2026-09-01 21:09:28 -0500

    Create Ejercicio 2 - André.py

---
+++ Ejercicio 2 /Ejercicio 2 - André.py
@@ -0,0 +1,12 @@
+#Ejercio 2 - Mayor de Dos Números
+
+print("Bienvenido")
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+
+if num1 > num2:
+    print("El primer numero es el mayor")
+elif num2 > num1:
+    print("El segundo numero es el mayor")
+else:
+    print("Los dos numeros son iguales")
