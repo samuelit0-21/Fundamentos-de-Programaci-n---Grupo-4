a29ea8a3fd02f0ddc4bc74c48c9e9693f1d731c4
Samuelito_Pro <jfryxd@gmail.com>
2026-09-08 23:05:50 -0500

    corregimos error en las funciones

--- Ejercicio_3/Samuel_Milla.py
+++ Ejercicio_3/Samuel_Milla.py
@@ -1,5 +1,14 @@
-#LA FUNCIÓN
-def calcular_descuento(precio, porcentaje):
-    descuento = precio * (porcentaje / 100)
-    precio_final = precio - descuento        
-    return precio_final, descuento
+#Ejercicio 3: Calculadora de Promedio con Lista
+
+#    Escribe una función calcular_promedio(notas) que reciba una lista de notas y retorne el promedio, la nota
+#    mínima y la nota máxima. Además crea una función mostrar_resultado(nombre, notas) sin retorno que muestre
+#    un reporte formateado.
+
+
+def calcular_promedio(notas):
+    return sum(notas) / len(notas), min(notas), max(notas)
+
+def mostrar_resultado(nombre, notas):
+    prom, mn, mx = calcular_promedio(notas)
+    print(f"\nEstudiante: {nombre} | Promedio: {prom:.2f} | Mín: {mn} | Máx: {mx}")
+
