d540df9db1c5530fe13973882020a07a6a350ee1
Rodrigo <rodrigoalessandrg@gmail.com>
2026-09-08 22:36:05 -0500

    Creamos una calculadora de descuentos con def y return

---
+++ Ejercicio_1/Calculadora_de_descuentos.py
@@ -0,0 +1,31 @@
+# Ejercicio 1 : Calculadora de Descuento
+
+# Escribe una función llamada calcular_descuento(precio, porcentaje) que reciba el precio original de
+
+# un producto y el porcentaje de descuento, y retorne el precio final después del descuento. Luego
+
+# muestra el ahorro obtenido.
+
+def calcular_descuento(precio, porcentaje):
+  
+    descuento = precio * (porcentaje / 100)
+  
+    precio_final = precio - descuento     
+  
+    return precio_final, descuento
+
+# Hacemos la parte del pedido
+
+precio_producto = float(input("Ingrese el precio del producto: "))
+
+porcentaje_descuento = float(input("Ingrese el porcentaje de descuento: "))
+
+#Aplicamos la formula anterior
+
+pago_final, ahorro = calcular_descuento(precio_producto, porcentaje_descuento)
+
+# Mostranos el resultado final:
+
+print("El precio con descuento es:", pago_final)
+
+print("El ahorro obtenido es:", ahorro)
