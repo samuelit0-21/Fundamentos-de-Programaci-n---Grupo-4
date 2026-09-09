c19b337ab732727f2cbff7e7aabad7a73a900565
NinaDalessandro <nioxd16@gmail.com>
2026-09-08 23:16:58 -0500

    Creamos un verificador de números par e impar

---
+++ Ejercicio_2/Verificador_de_Numeros_par_e_impar.py
@@ -0,0 +1,23 @@
+# Ejercicio 2: Verificados de Número Par o Impar
+
+#  Crea una función es_par(numero) que retorne True si el número es par o False si es impar. Luego crea otra
+#  función mostrar_paridad(numero) (sin return) que use la primera función e imprima el resultado en pantalla
+#  con un mensaje.
+
+#LAS FUNCIONES
+def es_par(numero):
+    return numero % 2 == 0
+
+def mostrar_paridad(numero):
+    #Verificamos si el número tiene decimales
+    if numero != int(numero):
+        print(f"El número {numero} no es entero. La paridad solo aplica a enteros.")
+    elif es_par(numero):
+        print(f"El número {int(numero)} es PAR")
+    else:
+        print(f"El número {int(numero)} es IMPAR")
+
+#EL PROGRAMA PRINCIPAL
+num = float(input("Ingresa un número: "))
+
+mostrar_paridad(num)
