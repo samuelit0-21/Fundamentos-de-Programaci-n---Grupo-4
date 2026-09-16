c965a52cd00304d7129a09d40605fbe5f2de4eeb
Samuelito_Pro <jfryxd@gmail.com>
2026-09-15 21:40:11 -0500

    agregamos los comentarios

--- Variables-Locales-Ejercicios-01/Ejercicio1_Milla_Roldan.py
+++ Variables-Locales-Ejercicios-01/Ejercicio1_Milla_Roldan.py
@@ -1,12 +1,37 @@
-
 #Ejercicio-1
 
-def contar_vocales(texto):
-  vocales = "aeiouAEIOU"
-  conteo = 0 # local
-  for letra in texto:
-      if letra in vocales:
-          conteo += 1
-  return conteo
+# Creamos una función llamada contar_vocales.
+# El parámetro "texto" recibe la palabra o frase que queremos analizar.
 
-print(contar_vocales("Hola Mundo")) # 4
+def contar_vocales(texto):
+  
+  # Guardamos todas las vocales que queremos buscar.
+  # Esta variable es LOCAL porque existe solamente dentro de la función.
+  
+  vocales = "aeiouAEIOU"
+  
+    # Creamos un contador que comienza en 0.
+    # Aquí iremos acumulando la cantidad de vocales encontradas.
+  
+    conteo = 0
+
+    # Recorremos el texto letra por letra.
+
+    for letra in texto:
+
+        # Comprobamos si la letra actual está dentro de la cadena "vocales".
+      
+        if letra in vocales:
+
+            # Si encontramos una vocal, aumentamos el contador en 1.
+          
+            conteo += 1
+
+    # Al terminar de recorrer el texto, devolvemos la cantidad de vocales.
+
+    return conteo
+
+
+# Llamamos a la función enviándole el texto "Hola Mundo".
+# print() muestra en pantalla el resultado devuelto por la función.
+print(contar_vocales("Hola Mundo"))  # Resultado: 4
