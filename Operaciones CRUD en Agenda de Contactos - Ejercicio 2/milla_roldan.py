d096376f2506a09f4436258d84a79c94ff403117
Samuelito_Pro <jfryxd@gmail.com>
2026-09-22 18:45:15 -0500

    Creamos el enunciado del ejercicio 2

---
+++ Operaciones CRUD en Agenda de Contactos - Ejercicio 2/milla_roldan.py
@@ -0,0 +1,3 @@
+#Enunciado:
+#Tienes la agenda: ["Ana García", "Luis Torres", "Carlos Díaz", "María López"]
+#Realiza: (1) Agregar "Pedro Ruiz", (2) Buscar "Carlos Díaz" y mostrar posición, (3) Modificar "Luis Torres" por "Luis Mendoza", (4) Eliminar "Ana García".
