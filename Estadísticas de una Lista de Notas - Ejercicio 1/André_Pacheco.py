819c90c2e7e91ae860ad3825f86c2bc3e196238e
André Pacheco <chiposcorpion@gmail.com>
2026-09-22 19:15:14 -0500

    Creamos el ejercicio 1

--- Estadísticas de una Lista de Notas - Ejercicio 1/André_Pacheco.py
+++ Estadísticas de una Lista de Notas - Ejercicio 1/André_Pacheco.py
@@ -1,2 +1,2 @@
 #Dado el siguiente arreglo de notas: [15, 18, 12, 9, 17, 14, 20, 11, 16, 13]
-#Escribir un programa que calcule: promedio, nota más alta, nota más baja y cuántos aprobaron (nota ≥ 11)
+#Escribir un programa que calcule: promedio, nota más alta, nota más baja y cuántos aprobaron (nota ≥ 11) 
